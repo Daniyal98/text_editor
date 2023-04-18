09183ca9726f4b604d7fab8bf0555f0d141771ad
Daniyal <daniyalishfaq10@gmail.com>
2023-04-18 05:47:29 +0500

    error handling

--- text_editor.c
+++ text_editor.c
@@ -2,32 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <errno.h>
 #include <unistd.h>
 
 struct termios orig_termios;
 
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
 
 int main()
 {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
@@ -36,6 +52,8 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q')
+            break;
     }
     return 0;
 }
