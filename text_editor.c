37fa09aa71f97c55343bc4199cc6f903564744ab
Daniyal <daniyalishfaq10@gmail.com>
2023-04-18 05:43:21 +0500

    disabling clt c and cltr v inputs

--- text_editor.c
+++ text_editor.c
@@ -16,7 +16,9 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -28,11 +30,11 @@ int main()
     {
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return 0;
